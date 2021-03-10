e265a110735df80d6b1bf7c262d84ef4d8addf66
Andre Hähnel <andrehaehnel@hotmail.de>
2021-03-10 11:48:31 +0100

    Initial

---
+++ src/hhnl.PlugIn.Host/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Loader;
+
+namespace hhnl.PlugIn.Host
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var t = new AssemblyDependencyResolver(Path.GetFullPath("../../../../hhnl.PlugIn.Tests.TestPlugin/bin/Debug/netstandard2.0/hhnl.PlugIn.Tests.TestPlugIn.dll"));
+           
+        }
+    }
+}
